7efb7e3b6c89ca1716e4fbdafe63d2b55a35cd39
joaquin lema <joaco.lema@hotmail.com>
2019-10-03 16:01:55 -0300

    agregamos configuracion de firestore en enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyAkW_BdBTT19DtpgzgH13uGKPPNoyJDsEA",
+    authDomain: "control-clientes-f376e.firebaseapp.com",
+    databaseURL: "https://control-clientes-f376e.firebaseio.com",
+    projectId: "control-clientes-f376e",
+    storageBucket: "control-clientes-f376e.appspot.com",
+    messagingSenderId: "538769636366",
+    appId: "1:538769636366:web:56a565996a1f178882d05f"
+  }
 };
