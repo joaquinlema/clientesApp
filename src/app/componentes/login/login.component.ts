ff9ffa377dcb19995adde47867b376c1503fcf16
joaquin lema <joaco.lema@hotmail.com>
2019-10-22 12:01:57 -0300

    formulario login con atributos

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(private router: Router,
+    private flashMessages: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  login() {
+
+  }
 }
